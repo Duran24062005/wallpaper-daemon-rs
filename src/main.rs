10bc04859191b57f38235f7253d39d9b4c208b1b
Alexi Dg <alexisdurangomez588@gmail.com>
2026-09-23 16:32:27 -0500

    feat: :fire: the project files structure has been added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod wallpapers;
+
 fn main() {
-    println!("Hello, world!");
+
+        // let output2 = Command::new("gsettings")
+        //     .arg("get")
+        //     .arg("org.gnome.desktop.background")
+        //     .arg("picture-uri")
+        //     .output()
+        //     .expect("Failed to execute command");
+
+        let wallpaper = wallpapers::get_current_wallpaper().expect("Failed to get current path");
+
+
+
+        println!("{wallpaper}");
 }
---
+++ src/wallpapers.rs
@@ -0,0 +1,31 @@
+use std::path::Path;
+use std::process::Command;
+
+pub fn get_current_wallpaper() -> Result<String, Box<dyn std::error::Error>> {
+    let output = Command::new("gsettings")
+        .args([
+            "get",
+            "org.gnome.desktop.background",
+            "picture-uri",
+        ])
+        .output()?;
+
+    let wallpaper = String::from_utf8(output.stdout)?;
+
+    Ok(wallpaper)
+}
+
+pub fn set_wallpaper(image: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let uri = format!("file://{}", image.canonicalize()?.display());
+
+    Command::new("gsettings")
+        .args([
+            "set",
+            "org.gnome.desktop.background",
+            "picture-uri",
+            &uri,
+        ])
+        .status()?;
+
+    Ok(())
+}
