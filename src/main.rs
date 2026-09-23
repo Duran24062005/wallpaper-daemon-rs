fac2791d0fc6c62e3e132bc8c2a9d791bbc50c2b
Alexi Dg <146484246+Duran24062005@users.noreply.github.com>
2026-09-23 16:58:15 -0500

    refactor: simplify wallpaper demo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
 mod wallpapers;
 
 fn main() {
+    let wallpaper = wallpapers::get_current_wallpaper()
+        .expect("Failed to get current wallpaper");
 
-        // let output2 = Command::new("gsettings")
-        //     .arg("get")
-        //     .arg("org.gnome.desktop.background")
-        //     .arg("picture-uri")
-        //     .output()
-        //     .expect("Failed to execute command");
-
-        let wallpaper = wallpapers::get_current_wallpaper().expect("Failed to get current path");
-
-
-
-        println!("{wallpaper}");
+    println!("Current wallpaper: {wallpaper}");
 }
