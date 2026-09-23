cb9faaa82fe1376b2540099fe4a55a39f954b7ba
Alexi Dg <alexisdurangomez588@gmail.com>
2026-09-23 17:16:32 -0500

    feat: :lock: the code_templates with comments in projects has been added

---
+++ src/code_templates/review.rs
@@ -0,0 +1,6 @@
+// let output2 = Command::new("gsettings")
+        //     .arg("get")
+        //     .arg("org.gnome.desktop.background")
+        //     .arg("picture-uri")
+        //     .output()
+        //     .expect("Failed to execute command");
---
+++ src/code_templates/wallpapers_review.rs
@@ -0,0 +1,44 @@
+use std::path::Path;
+use std::process::Command;
+
+pub fn get_current_wallpaper() -> Result<String, Box<dyn std::error::Error>> {
+    let output = Command::new("gsettings")
+        .args([
+            "get",
+            "org.gnome.desktop.background",
+            "picture-uri",
+        ])
+        .output()?;
+    
+    // println!("{:=^50}", "");
+    // println!("status: {}", output.status);
+    // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    // println!("{:=^50}", "");
+
+    let wallpaper = String::from_utf8(output.stdout)?;
+
+    let wallpaper = wallpaper.trim().trim_matches('\'');
+
+
+    Ok(wallpaper.to_string())
+}
+
+pub fn set_wallpaper(image: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let uri = format!("file://{}", image.canonicalize()?.display());
+
+    let status = Command::new("gsettings")
+        .args([
+            "set",
+            "org.gnome.desktop.background",
+            "picture-uri",
+            &uri,
+        ])
+        .status()?;
+
+    if !status.success() {
+        return Err(format!("gsettings failed with status: {status}").into());
+    }
+
+    Ok(())
+}
